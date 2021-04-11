b315dcb45eaa118a93c436503297e75f51c0363e
kpose <ganihujude@gmail.com>
2021-04-11 04:28:06 +0100

    restructure dir

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyBFiHY2n9JcqC-Ja4lAnxW7Ea4g-36Dyig",
+  authDomain: "cargram-72669.firebaseapp.com",
+  projectId: "cargram-72669",
+  storageBucket: "cargram-72669.appspot.com",
+  messagingSenderId: "590512117703",
+  appId: "1:590512117703:web:cb0720030906ddc811ebe2",
+  measurementId: "G-3TPNR6WVN7",
+};
+
+module.exports = config;
